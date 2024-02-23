3ac8c60478d9a4cca2d3ccaf44c57cdd1a18631e
April Polubiec <aprilpolubiec@gmail.com>
2024-02-23 13:21:24 +0000

    make some simple empty classes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # shared-mobility-game
+
+
+## Docstrings
+https://en.wikipedia.org/wiki/Javadoc#Structure_of_a_Javadoc_comment
---
+++ src/main/java/com/rideshare/Player.java
@@ -0,0 +1,20 @@
+package com.rideshare;
+
+/**
+ * @author      April Polubec <aprilpdev@gmail.com>
+ * <p>
+ * 
+ * <p>
+ */
+public class Player {
+    // Attributes
+    // xPosition
+    // yPosition
+    // avatar
+
+    // Methods
+    // moveLeft
+    // moveRight
+    // moveUp
+    // moveDown
+}
