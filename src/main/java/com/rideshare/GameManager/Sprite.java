a0a1ff4fa8c1311686155f05125086fbdcba7db2
April Polubiec <aprilpolubiec@gmail.com>
2024-02-24 14:02:34 +0000

    add sprite and test file

---
+++ src/main/java/com/rideshare/GameManager/Sprite.java
@@ -0,0 +1,8 @@
+package com.rideshare.GameManager;
+
+public class Sprite {
+    public Sprite(String name) {
+        // Get the sprite resource dir
+        // Import all
+    }
+}
---
+++ src/test/java/com/rideshare/GameManager/SpriteTest.java
@@ -0,0 +1,12 @@
+package com.rideshare.GameManager;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class SpriteTest {
+    @Test
+    void addition() {
+        assertTrue(true);
+    }
+}
