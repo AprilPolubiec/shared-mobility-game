ea4823bc75e805de89436f302f450d7db8d298c8
April Polubiec <aprilpolubiec@gmail.com>
2024-03-03 10:57:46 +0000

    create city tests

--- src/main/java/com/rideshare/City.java
+++ src/main/java/com/rideshare/City.java
@@ -1,15 +1,21 @@
 package com.rideshare;
 
-public class City {
-    //TODO: attributes
-    // Name
-    // Size
-    // Routes
-    // Difficulty level
-    // Mailboxes - list of type Mailbox
+/**
+ * Description: A city represents a location which can be visualized as a map. It contains various modes of transportation which can navigate on various routes to take people from one place to another.
+    Attributes:
+    Name (string): name to identify the city by (eg: “Dublin”)
+    Routes (Route[]): all the possible routes that can be traveled on in the city
+    DifficultyLevel (Integer): indicates the “difficulty” of the city, integer 0 - 10, where 0 is the easiest and 10 is the hardest.
+    Mailboxes (Mailbox[]): a list of Mailboxes which exist within this city
 
-    // TODO: methods
-    // createCity
-    // loadCity
-    // addMailboxes(count)
+    Methods:
+    constructor
+    createCity(): Creates the initial map
+    loadCity(): Prepares a city for gameplay and renders the map to be visible to the player
+    addMailboxes(int count): adds n mailboxes to the city
+    any other getters/setters needed
+ */
+
+public class City {
+    
 }
