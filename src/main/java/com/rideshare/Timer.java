b8957062decfd7c5f2c7968bfe340734c9637cc8
jmacap <156478354+jmacap@users.noreply.github.com>
2024-03-23 13:28:03 +0000

    Loose-grained super rudimentary timer created, with some functions defined, currently working on finer-grained implementations with the game-cycle set as 5 minutes in real life and 24 hours in game time.

--- src/main/java/com/rideshare/Timer.java
+++ src/main/java/com/rideshare/Timer.java
@@ -11,52 +11,122 @@ import java.time.LocalTime;
  */
 public class Timer {
     private TimerState state = TimerState.UNINITIALIZED;
-    private LocalTime currentTime;
+    private LocalTime currentInGameTime;
+    private LocalTime currentIRLTime;
+    private boolean isRunning = true;
+    private boolean isPaused = false;
 
+ 
     public Timer() {
-        // Initialize timer
-        throw new UnsupportedOperationException("Timer() not implemented yet");
+        currentInGameTime = LocalTime.MIDNIGHT;
+        currentIRLTime = LocalTime.MIDNIGHT;
     }
 
     public void initialize() {
-        // Should set the state to initialized
-        // Should set current time to 00:00
-        throw new UnsupportedOperationException("initialize() not implemented yet");
+        state = TimerState.INITIALIZED;
+        currentInGameTime = LocalTime.of(0, 0, 0);
     }
    
     public void start() {
+        isRunning = true;
+        currentIRLTime = LocalTime.now();
         // TODO:
         // Start a while loop, that runs until it is end of the day
         // The loop should check if one second has passed since it last ran
         // If one second has passed, it should increment the currentTime by one second
         // If the currentTime reaches the EOD, call stop()
-        while (!isEndOfDay()) {
-            // Do things!
+        
+        // Finer-grained implementation, Time Ratio: 1/288 seconds IRL, 1 minute in GameTime
+        //  5 minutes in IRL = 24 hours in GameTime
+
+        while ((!isEndOfDay()) && isRunning) {
+            // this initialises the 'counter' for right now! i.e. 12:30:01 
+            LocalTime counterTime = LocalTime.now();
+
+            // this checks if 1/288 second has passed 12:30:(1/288). 
+            // & if it is the case, increment the current game time by the amount we want i.e. 1 minute
+            // Then it sets currentIRL as equivalent to counterTime
+            // functionally updating the counterTime by 1/288
+            if (LocalTime.now().isAfter(currentIRLTime.plusNanos(3472222))) {
+                currentInGameTime = currentInGameTime.plusMinutes(1);
+                currentIRLTime = counterTime;
+                System.out.println("Current in-game time: " + currentInGameTime);
+                System.out.println("CurrentIRLTime:" + currentIRLTime);
+            }
+            try {
+                    Thread.sleep(100); // Sleep for 100 milliseconds
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             if (isEndOfDay()) {
                 stop();
-            }
-        }
-        throw new UnsupportedOperationException("start() not implemented yet");
+                }        
+        } 
+        
+    }
+        
+        
+        // Loose grained implementation, Time Ratio: 1 second IRL, 30 minutes in GameTime
+        // while (!isEndOfDay() && isRunning) {
+        //     LocalTime newTime = LocalTime.now();
+
+        //     if (LocalTime.now().isAfter(currentIRLTime.plusSeconds(1))){
+        //         // currentInGameTime = currentInGameTime.plusHours(1);
+        //         currentInGameTime = currentInGameTime.plusMinutes(30);
+        //         currentIRLTime = newTime;
+        //         System.out.println("Current in-game time: " + currentInGameTime);
+        //         System.out.println("CurrentIRLTime:" + currentIRLTime);
+        //     }
+        //     try {
+        //         Thread.sleep(100); // Sleep for 100 milliseconds
+        //     } catch (InterruptedException e) {
+        //         e.printStackTrace();
+        //     }
+            
+        //     if (isEndOfDay()) {
+        //         stop();
+        //     }
+        // }
+    // }
+
+    public void stop() {
+        state = TimerState.STOPPED;
+        isRunning = false;
+        
     }
 
     public void pause() {
-        throw new UnsupportedOperationException("pause() not implemented yet");
-    }
-
-    public void stop() {
-        throw new UnsupportedOperationException("stop() not implemented yet");
+        state = TimerState.PAUSED;
+        isRunning = false;
+        // throw new UnsupportedOperationException("pause() not implemented yet");
     }
 
     private boolean isEndOfDay() {
-        // Note: this is looking for an EXACT timestamp, in the loop above make sure we set the clock to MAX appropriately
-        return currentTime.compareTo(LocalTime.MAX) == 0;
+        
+        // loose-grained implementation of EndOfDay
+        // Checks if currentInGameTime is before midnight (i.e. checks if it's 23:00) &
+        // then checks if we add an hour equivalent to 00:00.
+        // boolean result = currentInGameTime.isBefore(LocalTime.MIDNIGHT) && currentInGameTime.plusHours(1).equals(checkForNextMidgnight);
+        
+        // return currentInGameTime.compareTo(LocalTime.MAX) == 0;
+        return currentInGameTime.equals(LocalTime.of(23, 59));
+
     }
 
     public LocalTime getTime() {
-        throw new UnsupportedOperationException("getTime() not implemented yet");
+        return currentInGameTime;
+
     }
 
     public TimerState getState() {
-        throw new UnsupportedOperationException("getState() not implemented yet");
+        return state;
+
     }
+
+    // Tester function since the TimerTest.java isn't working for me D:
+    public static void main(String[] args) {
+        Timer timer = new Timer();
+        timer.initialize();
+        timer.start();
+}
 }
