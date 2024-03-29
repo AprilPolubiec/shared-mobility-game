bc54503283c930a14372c719ea24731f0606b19b
jmacap <156478354+jmacap@users.noreply.github.com>
2024-03-29 12:26:41 +0000

    Class dev:Fine-grained implementation of timer updating nanoseconds IRL to 1 second in Game Time. Still troubleshooting pause button

--- src/main/java/com/rideshare/Timer.java
+++ src/main/java/com/rideshare/Timer.java
@@ -24,70 +24,70 @@ public class Timer {
 
     public void initialize() {
         state = TimerState.INITIALIZED;
-        currentInGameTime = LocalTime.of(0, 0, 0);
+        currentInGameTime = LocalTime.of(23, 00, 0);
     }
+
+            // Finer-grained implementation, Time Ratio: ? nanoseconds/1 second in gameTime
+        //  15 minutes in IRL = 24 hours in GameTime
+        
+            // this checks if a certain nanosecond has passed. 
+            // & if it is the case, increment the current game time by the amount we want i.e. 1 second
+            // Then it sets currentIRL as equivalent to counterTime
+            // functionally updating the counterTime by the nanoseconds added
+            //  3,472,200 nano seconds for five minute testing
+            // 10,416,666 for 15 minute testing 
    
     public void start() {
-        isRunning = true;
         currentIRLTime = LocalTime.now();
-        // TODO:
-        // Start a while loop, that runs until it is end of the day
-        // The loop should check if one second has passed since it last ran
-        // If one second has passed, it should increment the currentTime by one second
-        // If the currentTime reaches the EOD, call stop()
         
-        // Finer-grained implementation, Time Ratio: 1/288 seconds IRL, 1 minute in GameTime
-        //  5 minutes in IRL = 24 hours in GameTime
-
-        while ((!isEndOfDay()) && isRunning) {
+        while ((!isPaused && !isEndOfDay())) {
+            
             // this initialises the 'counter' for right now! i.e. 12:30:01 
             LocalTime counterTime = LocalTime.now();
 
-            // this checks if 1/288 second has passed 12:30:(1/288). 
-            // & if it is the case, increment the current game time by the amount we want i.e. 1 minute
-            // Then it sets currentIRL as equivalent to counterTime
-            // functionally updating the counterTime by 1/288
-            if (LocalTime.now().isAfter(currentIRLTime.plusNanos(3472222))) {
-                currentInGameTime = currentInGameTime.plusMinutes(1);
+            if (LocalTime.now().isAfter(currentIRLTime.plusNanos(3472200))) {
+                currentInGameTime = currentInGameTime.plusSeconds(1);
                 currentIRLTime = counterTime;
                 System.out.println("Current in-game time: " + currentInGameTime);
-                System.out.println("CurrentIRLTime:" + currentIRLTime);
+                System.out.println("CurrentIRLTime:" + currentIRLTime); }
+
             }
-            try {
-                    Thread.sleep(100); // Sleep for 100 milliseconds
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+
             if (isEndOfDay()) {
                 stop();
                 }        
         } 
-        
-    }
-        
-        
-        // Loose grained implementation, Time Ratio: 1 second IRL, 30 minutes in GameTime
-        // while (!isEndOfDay() && isRunning) {
-        //     LocalTime newTime = LocalTime.now();
 
-        //     if (LocalTime.now().isAfter(currentIRLTime.plusSeconds(1))){
-        //         // currentInGameTime = currentInGameTime.plusHours(1);
-        //         currentInGameTime = currentInGameTime.plusMinutes(30);
-        //         currentIRLTime = newTime;
-        //         System.out.println("Current in-game time: " + currentInGameTime);
-        //         System.out.println("CurrentIRLTime:" + currentIRLTime);
-        //     }
-        //     try {
-        //         Thread.sleep(100); // Sleep for 100 milliseconds
-        //     } catch (InterruptedException e) {
-        //         e.printStackTrace();
-        //     }
-            
-        //     if (isEndOfDay()) {
-        //         stop();
-        //     }
-        // }
+    
+// POSSIBLE SOLUTION PAUSE IS ONE SECOND AFTER CURRENT GAME TIME STORE THAT VALUE
+// RETURN THAT VALUE WHEN RESUME FUNCTION. SAME LOOP.
+
+    //     while ((!isEndOfDay()) && isRunning) {
+    //         // this initialises the 'counter' for right now! i.e. 12:30:01 
+    //         LocalTime counterTime = LocalTime.now();
+
+    //         // this checks if 1/288 second has passed 12:30:(1/288). 
+    //         // & if it is the case, increment the current game time by the amount we want i.e. 1 minute
+    //         // Then it sets currentIRL as equivalent to counterTime
+    //         // functionally updating the counterTime by 1/288
+    //         if (LocalTime.now().isAfter(currentIRLTime.plusNanos(3472222))) {
+    //             currentInGameTime = currentInGameTime.plusMinutes(1);
+    //             currentIRLTime = counterTime;
+    //             System.out.println("Current in-game time: " + currentInGameTime);
+    //             System.out.println("CurrentIRLTime:" + currentIRLTime);
+    //         }
+    //         try {
+    //                 Thread.sleep(100); // Sleep for 100 milliseconds
+    //             } catch (InterruptedException e) {
+    //                 e.printStackTrace();
+    //             }
+    //         if (isEndOfDay()) {
+    //             stop();
+    //             }        
+    //     } 
+        
     // }
+        
 
     public void stop() {
         state = TimerState.STOPPED;
@@ -95,11 +95,13 @@ public class Timer {
         
     }
 
-    public void pause() {
-        state = TimerState.PAUSED;
-        isRunning = false;
-        // throw new UnsupportedOperationException("pause() not implemented yet");
-    }
+    // public void pause() {
+    //     state = TimerState.PAUSED;
+    //     isPaused = true;
+    //     System.out.println("paused!");
+    //     stop();
+       
+    // }
 
     private boolean isEndOfDay() {
         
@@ -109,8 +111,12 @@ public class Timer {
         // boolean result = currentInGameTime.isBefore(LocalTime.MIDNIGHT) && currentInGameTime.plusHours(1).equals(checkForNextMidgnight);
         
         // return currentInGameTime.compareTo(LocalTime.MAX) == 0;
-        return currentInGameTime.equals(LocalTime.of(23, 59));
+        return currentInGameTime.equals(LocalTime.of(23, 59, 59));
 
+    }
+
+    private boolean pause() {
+        return currentInGameTime.equals(LocalTime)
     }
 
     public LocalTime getTime() {
@@ -128,5 +134,31 @@ public class Timer {
         Timer timer = new Timer();
         timer.initialize();
         timer.start();
+
+        try {
+            Thread.sleep(10000); // Sleep for 10 seconds
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        timer.pause();
+
+        // try {
+        //     Thread.sleep(5000); // Sleep for 5 seconds
+        // } catch (InterruptedException e) {
+        //     e.printStackTrace();
+        // }
+    
+        timer.start();
+        
+        // try {
+        //     Thread.sleep(3000); // Sleep for 3 seconds after resuming
+        // } catch (InterruptedException e) {
+        //     e.printStackTrace();
+        // }
+
+        timer.stop();
+
+
 }
 }
