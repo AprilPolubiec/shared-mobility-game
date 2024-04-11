720a8cf1e29c54f188f2269471cec55f92812f89
Ellen Doherty <ellenmdoherty@gmail.com>
2024-04-11 10:35:34 +0100

    map files
    maps 2-5

---
+++ src/main/resources/images/bg/mapTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="mapTiles" tilewidth="32" tileheight="32" tilecount="198" columns="18">
+ <image source="../tilesheet/tiles-32px.png" width="576" height="352"/>
+</tileset>
