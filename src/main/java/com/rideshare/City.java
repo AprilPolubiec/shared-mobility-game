40c9a211cf96d6ead7feb5a98673025656b32132
sadhbh23 <127418826+sadhbh23@users.noreply.github.com>
2024-04-11 14:19:14 +0100

    Cleaning Naming Convention: Renamed Instance Variables

--- src/main/java/com/rideshare/City.java
+++ src/main/java/com/rideshare/City.java
@@ -7,17 +7,17 @@ import javafx.geometry.Point2D;
 /**
  * Description: A city represents a location which can be visualized as a map. It contains various modes of transportation which can navigate on various routes to take people from one place to another.
     Attributes:
-    Name (string): name to identify the city by (eg: “Dublin”)
-    Routes (Route[]): all the possible routes that can be traveled on in the city
-    DifficultyLevel (Integer): indicates the “difficulty” of the city, integer 0 - 10, where 0 is the easiest and 10 is the hardest.
-    Mailboxes (Mailbox[]): a list of Mailboxes which exist within this city
-    Dimensions - what is the height/width of the city?
+      Name (string): name to identify the city by (eg: “Dublin”)
+      Routes (Route[]): all the possible routes that can be traveled on in the city
+      DifficultyLevel (Integer): indicates the “difficulty” of the city, integer 0 - 10, where 0 is the easiest and 10 is the hardest.
+      Mailboxes (Mailbox[]): a list of Mailboxes which exist within this city
+      Dimensions - what is the height/width of the city?
     
     Methods:
-    constructor - at your discretion
-    getters/setters - at your discretion
-    loadCity(): Prepares a city for gameplay and renders the map to be visible to the player (is this necessary?)
-    addMailboxes(int count): adds n mailboxes to the city
+      constructor - at your discretion
+      getters/setters - at your discretion
+      loadCity(): Prepares a city for gameplay and renders the map to be visible to the player (is this necessary?)
+      addMailboxes(int count): adds n mailboxes to the city
  */
 
 public class City {
--- src/main/java/com/rideshare/Mailbox.java
+++ src/main/java/com/rideshare/Mailbox.java
@@ -17,29 +17,29 @@ import javafx.scene.media.MediaPlayer;
 import javafx.util.Duration;
 
 public class Mailbox {
-   private int _row;
-   private int _col;
+   private final int row;
+   private final int col;
    MailboxStatus status = MailboxStatus.UNINITIALIZED;
-   boolean _isVisible = false;
-   TileManager _tileManager;
-   ImageView _mailboxTile;
-   int _mailboxTileImageIdx;
-   int _houseTileId;
-   int _duration; // In seconds
+   boolean isVisible = false;
+   TileManager tileManager;
+   ImageView mailboxTile;
+   int mailboxTileImageIdx;
+   int houseTileId;
+   int duration; // In seconds
    // DateTime startTime; // Maybe
    MediaPlayer mailboxWaitingAudio;
    MediaPlayer mailboxCompletedAudio;
-   TripCalculator _tripCalculator;
+   TripCalculator tripCalculator;
 
    public Mailbox(int row, int col, int houseTileId, TileManager tileManager) {
-      _row = row;
-      _col = col + 1; // Render a mailbox to the right of the house
-      _tileManager = tileManager;
-      _houseTileId = houseTileId;
+      this.row = row;
+      this.col = col + 1; // Render a mailbox to the right of the house
+      this.tileManager = tileManager;
+      this.houseTileId = houseTileId;
    }
 
    public void setTripCalculator(City city) {
-      _tripCalculator = new TripCalculator(city);
+      tripCalculator = new TripCalculator(city);
    }
 
    public void render() {
@@ -49,13 +49,13 @@ public class Mailbox {
       Media completedMedia = new Media(App.class.getResource("/images/audio/confirmation_001.mp3").toString());
       mailboxCompletedAudio = new MediaPlayer(completedMedia);
 
-      _mailboxTileImageIdx = 0;
-      this._mailboxTile = _tileManager.drawTile(_houseTileId + TileUtils.FLAG_HOUSE_OFFSET + _mailboxTileImageIdx, _row,
-            _col);
-      _mailboxTile.setOnMouseClicked(event -> {
+      mailboxTileImageIdx = 0;
+      this.mailboxTile = tileManager.drawTile(houseTileId + TileUtils.FLAG_HOUSE_OFFSET + mailboxTileImageIdx, row,
+            col);
+      mailboxTile.setOnMouseClicked(event -> {
          markComplete();
          // Path finder eek
-         _tripCalculator.calculateTrips(13, 13, _row, _col);
+         tripCalculator.calculateTrips(13, 13, row, col);
       });
       this.status = MailboxStatus.READY;
 
@@ -71,12 +71,12 @@ public class Mailbox {
             }
             timer.cancel();
          }
-      }, _duration * 1000); // Convert seconds to milliseconds
+      }, duration * 1000L); // Convert seconds to milliseconds
    }
 
    // TODO: actually just deduce from the house id
    public void setDuration(int duration) {
-      _duration = duration;
+      this.duration = duration;
    }
 
    public MailboxStatus getStatus() {
@@ -84,24 +84,24 @@ public class Mailbox {
    }
 
    public void show() {
-      this._isVisible = true;
-      this._mailboxTile.setOpacity(1);
+      this.isVisible = true;
+      this.mailboxTile.setOpacity(1);
    }
 
    public void hide() {
-      this._isVisible = false;
-      this._mailboxTile.setOpacity(0);
+      this.isVisible = false;
+      this.mailboxTile.setOpacity(0);
    }
 
    public void markComplete() {
-      _tileManager.drawTile(202, _row - 1, _col);
-      _tileManager.replaceTileImage(_mailboxTile, TileUtils.COMPLETED_FLAG_IDS[0]);
+      tileManager.drawTile(202, row - 1, col);
+      tileManager.replaceTileImage(mailboxTile, TileUtils.COMPLETED_FLAG_IDS[0]);
       this.status = MailboxStatus.COMPLETED;
       mailboxCompletedAudio.play();
    }
 
    public void markFailed() {
-      _tileManager.drawTile(201, _row - 1, _col);
+      tileManager.drawTile(201, row - 1, col);
       this.status = MailboxStatus.FAILED;
    }
 
