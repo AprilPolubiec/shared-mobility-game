ff8272d164c35a15662428b6528b73668ea239ea
April Polubiec <aprilpolubiec@gmail.com>
2024-04-21 08:40:06 +0100

    basic controller hooked up to the chooser

--- src/main/java/com/rideshare/Controllers/ChooseCharacterController.java
+++ src/main/java/com/rideshare/Controllers/ChooseCharacterController.java
@@ -2,13 +2,41 @@ package com.rideshare.Controllers;
 
 import java.io.IOException;
 
+import com.rideshare.Utils;
+
+import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class ChooseCharacterController {
     Stage stage;
     AnchorPane root;
+
+    @FXML
+    public ImageView spritePreview;
+    @FXML
+    public ImageView rightButton;
+    @FXML
+    public ImageView leftButton;
+    @FXML
+    public TextField nameInput;
+
+    @FXML
+    public void onRightButtonClicked() {
+        Utils.print("Right clicked");
+    }
+
+    @FXML
+    public void onLeftButtonClicked() {
+        Utils.print("Left clicked");
+    }
+    @FXML
+    public void onStartButtonClicked() {
+        Utils.print("Start clicked");
+    }
 
     private AnchorPane setScene(boolean isFullScreen) throws IOException {
         Scene scene = this.stage.getScene();
