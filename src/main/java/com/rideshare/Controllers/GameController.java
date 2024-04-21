d06d13e2234b92285a9a40971847bf49417435cf
jmacap <156478354+jmacap@users.noreply.github.com>
2024-04-21 19:49:40 +0100

    deleted all files requested

--- src/main/java/com/rideshare/Controllers/GameController.java
+++ src/main/java/com/rideshare/Controllers/GameController.java
@@ -60,19 +60,6 @@ public class GameController {
         _city = loader.getCity();
     }
 
-    // TODO: create a ProgressBar class!
-    // private void loadProgressModal() {
-    //     ProgressBar progressBar = new ProgressBar(0);
-    //     progressBar.setProgress(0.5);
-
-    //     VBox vBox = new VBox(progressBar);
-    //     progressBar.setPrefWidth(960);
-    //     progressBar.setPrefHeight(50);
-
-    //     progressBar.setStyle("-fx-accent: #fa8132;");
-    //     AnchorPane.setBottomAnchor(vBox, 0.0);
-    //     _root.getChildren().add(vBox);
-    // }
 
     @FXML
     public void start() {
