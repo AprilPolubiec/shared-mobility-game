f71f4a8be7d25d28388bd75538f50e89f4cf4808
April Polubiec <aprilpolubiec@gmail.com>
2024-04-23 21:38:50 +0100

    clean up mailbox route and popup tests

--- src/test/java/com/rideshare/City/MailboxTest.java
+++ src/test/java/com/rideshare/City/MailboxTest.java
@@ -1,105 +1,17 @@
 package com.rideshare.City;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
-
-import com.rideshare.City.City.Mailbox;
-import com.rideshare.City.City.MailboxStatus;
-import com.rideshare.GameManager.TimerState;
+import com.rideshare.TileManager.GridPanePosition;
 
 public class MailboxTest {
     @Test
     void Test_CanCreateMailbox() {
-        int positionX = 0;
-        int positionY = 0;
-        int duration = 5;
-        Mailbox mailbox = new Mailbox(positionX, positionY, duration);
-    
-        assertEquals(this.positionX, mailbox.getPosition().x);
-        assertEquals(this.positiony, mailbox.getPosition().y);
-        assertEquals(MailboxStatus.READY, mailbox.getStatus());
-        assertNotNull(mailbox.getTimer());
-    }
+        Mailbox mailbox = new Mailbox(new GridPanePosition(1, 0), 0, null);
 
-    @Test
-    void Test_CanHideMailbox() {
-        int positionX = 0;
-        int positionY = 0;
-        int duration = 5;
-        Mailbox mailbox = new Mailbox(positionX, positionY, duration);
-        // We are defauling to hiding but can be showing - up to you
-        assertEquals(false, mailbox.IsVisible);
-        mailbox.show();
-        assertEquals(true, mailbox.IsVisible);
-        assertEquals(MailboxStatus.WAITING, mailbox.getStatus());
-        mailbox.hide();
-        assertEquals(false, mailbox.IsVisible);
-        assertEquals(MailboxStatus.READY, mailbox.getStatus());
-    }
-
-    @Test
-    void Test_CanMarkMailboxComplete() {
-        int positionX = 0;
-        int positionY = 0;
-        int duration = 5;
-        Mailbox mailbox = new Mailbox(positionX, positionY, duration);
-        mailbox.show();
-        mailbox.markComplete();
-        assertEquals(MailboxStatus.COMPLETED, mailbox.getStatus());
-    }
-
-    @Test
-    void Test_CannotMarkHiddenMailboxComplete() {
-        int positionX = 0;
-        int positionY = 0;
-        int duration = 5;
-        Mailbox mailbox = new Mailbox(positionX, positionY, duration);
-        mailbox.hide();
-        assertThrows(Exception.class, () -> mailbox.markComplete());
-    }
-
-    @Test
-    void Test_MailboxTimerStartsOnShow() {
-        int positionX = 0;
-        int positionY = 0;
-        int duration = 5;
-        Mailbox mailbox = new Mailbox(positionX, positionY, duration);
-        mailbox.show();
-        assertEquals(TimerState.RUNNING, mailbox.getTimer());
-    }
-
-    @Test
-    void Test_MailboxTimerPausesOnHide() {
-        int positionX = 0;
-        int positionY = 0;
-        int duration = 5;
-        Mailbox mailbox = new Mailbox(positionX, positionY, duration);
-        mailbox.show();
-        mailbox.hide();
-        assertEquals(TimerState.PAUSED, mailbox.getTimer());
-
-    }
-    @Test
-    void Test_MailboxTimerStopsOnComplete() {
-        int positionX = 0;
-        int positionY = 0;
-        int duration = 5;
-        Mailbox mailbox = new Mailbox(positionX, positionY, duration);
-        mailbox.show();
-        mailbox.markComplete();
-        assertEquals(TimerState.STOPPED, mailbox.getTimer());
-    }
-
-    @Test
-    void Test_CanGetMailboxStartTime() {
-        int positionX = 0;
-        int positionY = 0;
-        int duration = 5;
-        Mailbox mailbox = new Mailbox(positionX, positionY, duration);
-        mailbox.show();
-        assertNotNull(mailbox.getStartTime());
+        assertEquals(1, mailbox.getGridPanePosition().col);
+        assertEquals(1, mailbox.getGridPanePosition().row);
+        assertTrue(mailbox.isReady());
     }
 }
