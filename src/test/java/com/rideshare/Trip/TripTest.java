15cbf72ac1657e49c794d4ac30bb121203a2906f
April Polubiec <aprilpolubiec@gmail.com>
2024-04-23 21:43:26 +0100

    fix trip test

--- src/test/java/com/rideshare/Trip/TripTest.java
+++ src/test/java/com/rideshare/Trip/TripTest.java
@@ -1,26 +1,28 @@
 package com.rideshare.Trip;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import com.rideshare.City.RouteNodeMatrix;
+import com.rideshare.TileManager.GridPanePosition;
 import com.rideshare.TransportationMode.TransportationType;
-import com.rideshare.Trip.TransportationNode;
-import com.rideshare.Trip.Trip;
-import com.rideshare.Trip.TripType;
-
 public class TripTest {
+    // TODO: calculations have changed
     @Test
     public void Test_CanCreateTrip() {
-       // Create simple node list of walking
-       TransportationNode node1 = new TransportationNode(3, 0, TransportationType.WALKING, null);
-       TransportationNode node2 = new TransportationNode(2, 0, TransportationType.WALKING, null);
-       TransportationNode node3 = new TransportationNode(1, 0, TransportationType.WALKING, null);
-       TransportationNode node4 = new TransportationNode(0, 0, TransportationType.WALKING, null);
-       node4.parent = node3;
-       node3.parent = node2;
-       node2.parent = node1;
+        // Create simple node list of walking
+        TransportationNode node1 = new TransportationNode(new GridPanePosition(3, 0), TransportationType.WALKING,
+                new RouteNodeMatrix(new int[5][5], TransportationType.WALKING, "Walking"));
+        TransportationNode node2 = new TransportationNode(new GridPanePosition(2, 0), TransportationType.WALKING,
+                new RouteNodeMatrix(new int[5][5], TransportationType.WALKING, "Walking"));
+        TransportationNode node3 = new TransportationNode(new GridPanePosition(1, 0), TransportationType.WALKING,
+                new RouteNodeMatrix(new int[5][5], TransportationType.WALKING, "Walking"));
+        TransportationNode node4 = new TransportationNode(new GridPanePosition(0, 0), TransportationType.WALKING,
+                new RouteNodeMatrix(new int[5][5], TransportationType.WALKING, "Walking"));
+        node4.parent = node3;
+        node3.parent = node2;
+        node2.parent = node1;
 
         Trip t = new Trip(node4, node1, TripType.EFFICIENT);
 
@@ -31,27 +33,27 @@ public class TripTest {
 
     @Test
     public void Test_CanCalculateMultipleTransportationTypes() {
-       // Create simple node list of walking
-       TransportationNode node1 = new TransportationNode(3, 0, TransportationType.WALKING, null);
-       TransportationNode node11 = new TransportationNode(3, 0, TransportationType.WALKING, null);
-       node11.parent = node1;
-       TransportationNode node2 = new TransportationNode(2, 0, TransportationType.TRAIN, null);
-       node2.parent = node11;
-       TransportationNode node21 = new TransportationNode(2, 0, TransportationType.TRAIN, null);
-       node21.parent = node2;
-       TransportationNode node3 = new TransportationNode(1, 0, TransportationType.BUS, null);
-       node3.parent = node21;
-       TransportationNode node31 = new TransportationNode(1, 0, TransportationType.BUS, null);
-       node31.parent = node3;
-       TransportationNode node4 = new TransportationNode(0, 0, TransportationType.CAR, null);
-       node4.parent = node31;
-       TransportationNode node41 = new TransportationNode(0, 0, TransportationType.CAR, null);
-       node41.parent = node4;
-
+        // Create simple node list of walking
+        TransportationNode node1 = new TransportationNode(new GridPanePosition(3, 0), TransportationType.WALKING, new RouteNodeMatrix(new int[5][5], TransportationType.WALKING, "Test"));
+        TransportationNode node11 = new TransportationNode(new GridPanePosition(3, 0), TransportationType.WALKING,
+                new RouteNodeMatrix(new int[5][5], TransportationType.WALKING, "Test"));
+        node11.parent = node1;
+        TransportationNode node2 = new TransportationNode(new GridPanePosition(2, 0), TransportationType.TRAIN, new RouteNodeMatrix(new int[5][5], TransportationType.TRAIN, "Test"));
+        node2.parent = node11;
+        TransportationNode node21 = new TransportationNode(new GridPanePosition(2, 0), TransportationType.TRAIN, new RouteNodeMatrix(new int[5][5], TransportationType.TRAIN, "Test"));
+        node21.parent = node2;
+        TransportationNode node3 = new TransportationNode(new GridPanePosition(1, 0), TransportationType.BUS, new RouteNodeMatrix(new int[5][5], TransportationType.BUS, "Test"));
+        node3.parent = node21;
+        TransportationNode node31 = new TransportationNode(new GridPanePosition(1, 0), TransportationType.BUS, new RouteNodeMatrix(new int[5][5], TransportationType.BUS, "Test"));
+        node31.parent = node3;
+        TransportationNode node4 = new TransportationNode(new GridPanePosition(0, 0), TransportationType.CAR, new RouteNodeMatrix(new int[5][5], TransportationType.CAR, "Test"));
+        node4.parent = node31;
+        TransportationNode node41 = new TransportationNode(new GridPanePosition(0, 0), TransportationType.CAR, new RouteNodeMatrix(new int[5][5], TransportationType.CAR, "Test"));
+        node41.parent = node4;
 
         Trip t = new Trip(node41, node1, TripType.EFFICIENT);
         // Walking = 5km/hr = 2 / 5 * 60 = 24
-        // Train = 30 km/hr = 2 / 30 * 60 =  4
+        // Train = 30 km/hr = 2 / 30 * 60 = 4
         // Bus = 40 km/hr = 2/40 * 60 = 3
         // Car = 48 km/hr = 2/48 * 60 = 2.5
 
