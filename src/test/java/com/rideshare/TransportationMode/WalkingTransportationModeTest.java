c0ee67be4af35b8c115570180f350511d90421bf
April Polubiec <aprilpolubiec@gmail.com>
2024-04-23 21:09:17 +0100

    transportation mode tests work

--- src/test/java/com/rideshare/TransportationMode/WalkingTransportationModeTest.java
+++ src/test/java/com/rideshare/TransportationMode/WalkingTransportationModeTest.java
@@ -4,9 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import com.rideshare.TransportationMode.TransportationMode;
-import com.rideshare.TransportationMode.TransportationMode.WalkingTransportationMode;
-
 public class WalkingTransportationModeTest {
     @Test 
     public void Test_CanCreateWalkingTransportationMode(){;
