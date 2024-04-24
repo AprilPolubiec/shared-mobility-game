6dbc0ad37ef4466be8cdafcfecadca072af28c01
April Polubiec <aprilpolubiec@gmail.com>
2024-04-24 19:44:10 +0100

    fix all transportation mode tests

--- src/test/java/com/rideshare/TransportationMode/BusTransportationModeTest.java
+++ src/test/java/com/rideshare/TransportationMode/BusTransportationModeTest.java
@@ -10,8 +10,8 @@ public class BusTransportationModeTest {
         String displayName = "CitiBus";
         TransportationMode tm = new BusTransportationMode(displayName);
         
-        assertEquals(0, tm.getEmissionRate());
-        assertEquals(5, tm.getSpeed());
+        assertEquals(105, tm.getEmissionRate());
+        assertEquals(40, tm.getSpeed());
         assertEquals(displayName, tm.getName());
         assertEquals(true, tm.hasStops());
     }
